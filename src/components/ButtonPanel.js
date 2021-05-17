e69cd482b5da265a777cd767a6a3857b36b36dbe
okikiola11 <okikiola.apelehin@gmail.com>
2021-05-17 22:16:41 +0000

    Implement the ButtonPanel component Render the calculator panel with all the buttons Use divs to divide buttons into groups

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,5 +1,39 @@
 const ButtonPanel = () => (
-  <button type="submit"> Display Buttons </button>
+  <main>
+    <div>
+      <button name="symbolReset" type="submit"> AC </button>
+      <button name="symbolCalc" type="submit"> +/- </button>
+      <button name="symbolModulo" type="submit"> % </button>
+      <button name="symbolDiv" type="submit"> ÷ </button>
+    </div>
+
+    <div>
+      <button name="number7" type="submit"> 7 </button>
+      <button name="number8" type="submit"> 8 </button>
+      <button name="number9" type="submit"> 9 </button>
+      <button name="symbolMul" type="submit"> X </button>
+    </div>
+
+    <div>
+      <button name="number4" type="submit"> 4 </button>
+      <button name="number5" type="submit"> 5 </button>
+      <button name="number6" type="submit"> 6 </button>
+      <button name="symbolSubt" type="submit"> - </button>
+    </div>
+
+    <div>
+      <button name="number1" type="submit"> 1 </button>
+      <button name="number2" type="submit"> 2 </button>
+      <button name="number3" type="submit"> 3 </button>
+      <button name="symbolAdd" type="submit"> + </button>
+    </div>
+
+    <div>
+      <button name="number0" type="submit"> 0 </button>
+      <button name="symbolDec" type="submit"> . </button>
+      <button name="symbolEquals" type="submit"> = </button>
+    </div>
+  </main>
 );
 
 export default ButtonPanel;
