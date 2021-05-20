322b99b486e971a59faeef0f76e784bd0329f13a
okikiola11 <okikiola.apelehin@gmail.com>
2021-05-20 14:36:22 +0000

    Implement calculate.js calculate method should accept a calculator data object and a button name as arguments calculator data object should contain three properties: total, next and operation Implement the operate.js module The operate function accept numberOne, numberTwo and operation as arguments

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import DisplayCalculator from './DisplayCalculator';
 import ButtonPanel from './ButtonPanel';
+import Calculate from '../logic/calculate';
 
 const App = () => (
-  <div className="App">
-    <>
-      <DisplayCalculator />
-      <ButtonPanel />
-    </>
-  </div>
+  // <div className="App">
+  <>
+    <DisplayCalculator />
+    <ButtonPanel />
+    <Calculate />
+  </>
+  // </div>
 );
 
 export default App;
--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,13 +1,29 @@
+import Operate from './operate';
+
 const Calculate = (dataObj, buttonName) => {
   let { total, next, operation } = dataObj;
-  //const operations = ['+', '/', '*', '-'];
-  
-  if (buttonName == "+/-") {
-    total *= -1
-    next *= -1 
-  }
-  
-  console.log(total *= -1)
-}
+  const operators = ['+', '÷', 'x', '-', '%'];
 
-Calculate({total: 3, next: 5, operation: '+/-'})
+  if (buttonName === '+/-') {
+    total *= -1;
+    next *= -1;
+  } else if (buttonName === 'AC') {
+    total = null;
+    next = null;
+    operation = null;
+  } else if (operators.includes(buttonName)) {
+    Operate(total, next, operation);
+  } else if (buttonName === '.' && next) {
+    if (!next.includes('.')) {
+      next = next.concat('.');
+    }
+  } else if ((buttonName === '=') && (next && total)) {
+    total = Operate(total, next, operation);
+    next = null;
+    operation = null;
+  }
+
+  return [total, next, operation];
+};
+
+export default Calculate;
