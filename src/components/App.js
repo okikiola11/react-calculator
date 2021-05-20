afc537831bb016008c2d50aa697b98e5fed0765f
okikiola11 <okikiola.apelehin@gmail.com>
2021-05-20 14:38:03 +0000

    Remove commented code from App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,13 @@ import ButtonPanel from './ButtonPanel';
 import Calculate from '../logic/calculate';
 
 const App = () => (
-  // <div className="App">
-  <>
-    <DisplayCalculator />
-    <ButtonPanel />
-    <Calculate />
-  </>
-  // </div>
+  <div className="App">
+    <>
+      <DisplayCalculator />
+      <ButtonPanel />
+      <Calculate />
+    </>
+  </div>
 );
 
 export default App;
