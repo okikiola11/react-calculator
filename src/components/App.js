3220b63428ce8188af3d08b60bad4123413ae36f
okikiola11 <okikiola.apelehin@gmail.com>
2021-05-26 14:29:42 +0000

    Initialize state in the constructor with three properties: total, next and operation in App.js Implement render function that to display Display.js and ButtonPanel.js Implement the handleClick function to buttonName as an argument Pass handleClick clickHandler and result property to ButtonPanel and display component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
 import DisplayCalculator from './DisplayCalculator';
 import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
 
-const App = () => (
-  <>
-    <DisplayCalculator />
-    <ButtonPanel />
-  </>
-);
+class App extends React.Compnent {
+  constructor() {
+    super()
+    
+    this.state = {
+      total: null,
+      next: null,
+      operation: null
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const result = calculate(this.state, buttonName)
+    this.setState(result);
+  }
+
+  render() {
+    return (
+      <>
+        <DisplayCalculator result={result} />
+        <ButtonPanel onClick={this.handleClick} />
+      </>
+    )
+  }
+};
 
 export default App;
