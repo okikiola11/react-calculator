2bd607e2746e73cba1025be201bfcc43b304b7ea
okikiola11 <okikiola.apelehin@gmail.com>
2021-06-03 23:58:53 +0000

    Refactor code

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DisplayCalculator from './DisplayCalculator';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
+  //   this.state = {
+  //     total: null,
+  //     next: null,
+  //     operation: null,
+  //   };
+  // }
+
+  const [value, setValue] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  })
 
   handleClick = (buttonName) => {
     const result = calculate(this.state, buttonName);
